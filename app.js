bd68732320a91dbdc7296788c3543a6ec5f79895
FacundoEsquivel97 <esquivelfacundo.97@gmail.com>
2023-05-14 19:01:15 -0300

    Update!

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
 document.addEventListener( "DOMContentLoaded",()=>{
   const pokeName = document.querySelector('.pokeName');
   const pokeImg = document.querySelector('.pokeImg');
-  const inputHeight = document.querySelector('.inputHeight');
+  const pokeType = document.querySelector('.pokeType');
+  const pokeHeight = document.querySelector('.pokeHeight strong');
+  const textBox = document.querySelector('.textBox');
+ 
   const inputWeight = document.querySelector('.inputWeight');
   const inputSubmit = document.querySelector('.inputSubmit');
     
+  const redondearDecimales = (numero, decimales) => {
+    numeroRegexp = new RegExp('\\d\\.(\\d){' + decimales + ',}');   // Expresion regular para numeros con un cierto numero de decimales o mas
+    if (numeroRegexp.test(numero)) {         // Ya que el numero tiene el numero de decimales requeridos o mas, se realiza el redondeo
+        return Number(numero.toFixed(decimales));
+    } else {
+        return Number(numero.toFixed(decimales)) === 0 ? 0 : numero;  // En valores muy bajos, se comprueba si el numero es 0 (con el redondeo deseado), si no lo es se devuelve el numero otra vez.
+    }
+  }
+  const mostrarAlerta = (mensaje) => {
+    textBox.textContent = mensaje
+  }
+
+
+
   const renderPokemon = () => {
   let num = Math.floor(Math.random() * (151 - 1)) + 1;
   fetch('https://pokeapi.co/api/v2/pokemon/'+ num)
   .then (response => response.json())
   .then (pokemon => {
+    console.log(pokemon)
     console.log(pokemon.name);
-    console.log(pokemon.height/10)
+    const peso = pokemon.weight/10
+    console.log(peso)
+    pokemon.types.forEach((type)=>{
+     let typeBox = document.createElement('div')
+     typeBox.textContent = type.type.name
+     typeBox.className = 'typeBox'
+     pokeType.appendChild(typeBox)
+    })
     pokeName.textContent = pokemon.name;
+    pokeHeight.textContent = pokemon.height*10;
+    
     pokeImg.setAttribute('src',pokemon.sprites.other.home.front_default);
     
     inputSubmit.addEventListener('click',(e)=>{
       e.preventDefault()
-      let result = inputHeight.value - (pokemon.height/10);
+      let result = inputWeight.value - peso;
       if (result == 0) {alert('you win')} else {
-        alert(result)
+        mostrarAlerta('Haz fallado por ' + Math.abs(redondearDecimales(result,2)) + 'kg. El peso de este pokemon es ' + peso + 'kg.')
       }
     })
   })
